31fe826222256fa5e6e187d6bc844871ee8aa6aa
Sandra <sandra.moutous@gmail.com>
2026-08-09 14:40:37 +0200

    Display notice on flash

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  document.querySelectorAll(".flash").forEach((el) => {
+    setTimeout(() => el.remove(), 3000)
+  })
+})
