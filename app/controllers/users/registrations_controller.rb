84167e9724dbf46c4c8cbd5594c17a3688e8c797
Sandra <sandra.moutous@gmail.com>
2026-08-11 11:25:26 +0200

    Fix footer at the bottom of the page + reduce title for policies + redirect to children list after registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
+  def after_sign_up_path_for(_resource)
+    children_path
+  end
+
   def after_update_path_for(_resource)
     user_path
   end
