16028beccda824fe6ba7fa0e4c8453e7a346560d
Sandra <sandra.moutous@gmail.com>
2026-08-31 23:30:09 +0200

    feat: redesign word list cards with icon actions

---
+++ app/helpers/icons_helper.rb
@@ -0,0 +1,21 @@
+module IconsHelper
+  ICONS = {
+    pencil: '<path d="M12 20h9"/><path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4Z"/>',
+    trash:  '<path d="M3 6h18"/><path d="M8 6V4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/><path d="M10 11v6"/><path d="M14 11v6"/>',
+    eye:    '<path d="M2 12s3.5-7 10-7 10 7 10 7-3.5 7-10 7-10-7-10-7Z"/><circle cx="12" cy="12" r="3"/>',
+    plus:   '<path d="M12 5v14"/><path d="M5 12h14"/>',
+    play:   '<path d="M6 4l14 8-14 8V4Z"/>',
+    settings: '<circle cx="12" cy="12" r="3"/><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.6a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 1 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1Z"/>',
+    menu:     '<path d="M3 6h18"/><path d="M3 12h18"/><path d="M3 18h18"/>',
+    chevron:  '<path d="M6 9l6 6 6-6"/>'
+  }.freeze
+
+  def icon(name, size: 20, css_class: nil)
+    tag.svg(width: size, height: size, viewBox: "0 0 24 24",
+            fill: "none", stroke: "currentColor", stroke_width: 2,
+            stroke_linecap: "round", stroke_linejoin: "round",
+            class: css_class, aria_hidden: true) do
+      ICONS.fetch(name.to_sym).html_safe
+    end
+  end
+end
